8c785e2efaebb0f832dd6a4b28390d9789fcf000
anunay2021 <anunay.anindya@khoros.com>
2021-09-02 17:21:39 +0530

    #7.1 event handling

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("Hello");
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("Hello " + name);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button> {/* // just pass reference, don't call it by adding () as handleClick() */}
+            {/* <button onClick={handleClickAgain('mario')}>Click me again</button>  this will fire wout clicking and should be done as next line, wrapped in an anonymous function */}
+            <button onClick={() => handleClickAgain('mario')}>Click me again</button> 
         </div>
     );
 }
