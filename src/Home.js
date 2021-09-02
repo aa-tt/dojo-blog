cb541c03727a34865fcfab6a3b384b8526d0987e
anunay2021 <anunay.anindya@khoros.com>
2021-09-02 17:37:04 +0530

    #8.3 multi useState

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,18 @@ import { useState } from 'react';
 
 const Home = () => {
     // let name = 'mario';
-    const [name, setName] = useState('mario'); // useState hook function that returns initial value supplied to it (which is mario here) and a callback function to change state of variable 'name' in template
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
 
     const handleClick = () => {
         setName('luigi');
+        setAge(30);
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <p>{ name }</p>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click Me</button>
         </div>
     );
