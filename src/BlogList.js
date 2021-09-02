733fbb08a9c5acbe9766c69caef58c1600f98857
anunay2021 <anunay.anindya@khoros.com>
2021-09-02 18:49:30 +0530

    #13 Functions as Props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,14 @@
-const BlogList = ({blogs, title}) => {
+const BlogList = ({blogs, title, handleDelete}) => {
 
+    // const handleDelete = (id) => { } // writing a handleDelete here will only delete from blogs here but wont delete from actual source array which is in Home.js. So pass this function as prop to be used as callback
     return (
         <div className="blog-list">
             <h2>{title}</h2>
             {blogs.map(blog => (
                 <div className="blog-preview" key={blog.id} >
                 <h2>{ blog.title }</h2>
-                <p>Written by { blog.author }</p>
+                    <p>Written by {blog.author}</p>
+                    <button onClick={()=>handleDelete(blog.id)}>delete</button>
                 </div>
             ))}
         </div>
