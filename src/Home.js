fbe2455574515eb3a6bc27c19bd983ea98c0b732
anunay2021 <anunay.anindya@khoros.com>
2021-09-02 18:55:37 +0530

    #14 useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -14,10 +14,14 @@ const Home = () => {
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        console.log('useEffect fetch data, auth, etc..');
+        console.log(blogs);
+    });
+
     return (
         <div className="home">
             <BlogList blogs={ blogs } title="All Blogs" handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" handleDelete={handleDelete}/>
         </div>
     );
 }
