d045ea769bbfdea00d99f179ee954f30a2942ff5
anunay2021 <anunay.anindya@khoros.com>
2021-09-03 13:24:13 +0530

    #18 conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -11,17 +12,21 @@ const Home = () => {
     }
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs') // this returns a promise, so resolve with `then`
-            .then(res => {
-                return res.json(); // this converts json into javascript object and returns a promise, so resolve with `then`
-            })
-            .then((data) => {
-                setBlogs(data);
-            })
+        setTimeout(() => { // settimeout to 1 sec, just to simulate a real call on internet
+            fetch('http://localhost:8000/blogs') // this returns a promise, so resolve with `then`
+                .then(res => {
+                    return res.json(); // this converts json into javascript object and returns a promise, so resolve with `then`
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsPending(false);
+                })
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
         </div>
     );
