d2699bef1332228e816c951c6443e11c758205d5
anunay2021 <anunay.anindya@khoros.com>
2021-09-03 15:51:04 +0530

    #28 submitting a form

--- src/Create.js
+++ src/Create.js
@@ -5,11 +5,16 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
 
+    const handleSubmit = (e) => {
+        e.preventDefault(); // to stop default refresh on submit form
+        const blog = { title, body, author };
+        console.log(blog);
+    }
 
     return (
         <div className="create">
             <h2>Add a new blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input
                     type="text"
