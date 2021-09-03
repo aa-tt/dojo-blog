1297fe041755631108f2225058584e7b321b0905
anunay2021 <anunay.anindya@khoros.com>
2021-09-03 14:01:26 +0530

    #20 custom hook - for fetching data - making fetch logic reusable

--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,14 @@
-import { useState , useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null)
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
-    useEffect(() => {
-        setTimeout(() => { // settimeout to 1 sec, just to simulate a real call on internet
-            fetch('http://localhost:8000/blogss') // this returns a promise, so resolve with `then`
-                .then(res => {
-                    if (!res.ok) {
-                        throw Error(`data not fetched - ${res.status} ${res.statusText}`);
-                    }
-                    return res.json(); // this converts json into javascript object and returns a promise, so resolve with `then`
-                })
-                .then((data) => {
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch(err => {
-                    setError(err.message);
-                    setIsPending(false);
-                })
-        }, 1000);
-    }, []);
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs') // get `data` and call it as `blogs` in the context here
 
     return (
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
 }
---
+++ src/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => { // pass url to this hook function
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null)
+    
+    useEffect(() => {
+        setTimeout(() => { // settimeout to 1 sec, just to simulate a real call on internet
+            fetch(url) // this returns a promise, so resolve with `then`
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error(`data not fetched - ${res.status} ${res.statusText}`);
+                    }
+                    return res.json(); // this converts json into javascript object and returns a promise, so resolve with `then`
+                })
+                .then((data) => {
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setError(err.message);
+                    setIsPending(false);
+                })
+        }, 1000);
+    }, [url]); // rerun on change of dependency, which is `url`
+
+    return { data, isPending, error }; // return from this hook function a meaningful object
+}
+
+export default useFetch;
